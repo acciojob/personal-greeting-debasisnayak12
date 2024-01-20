f9ec37dad1685687596c2df00c581ac4d344490a
Debasis Nayak <debasishnayak0739@gmail.com>
2024-01-20 11:27:44 +0530

    submitting code-Sat Jan 20 2024 11:27:41 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName] = useState();
+
+  function handleInput(e){
+    setName(e.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form>
+        <p>Enter your name:</p>
+        <input type="text" onChange={handleInput} />
+      </form>
+
+      { name!=""? <p>Hello {name}</p>  : ""   }
+
     </div>
-  )
+  );
 }
 
 export default App
